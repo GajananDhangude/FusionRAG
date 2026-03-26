8a42ac582f7968176413642e6ad98c5055afa3e0
GajananDhangude <dhangudegajanan27@gmail.com>
2026-03-26 18:11:12 +0530

    semantic dense retriever

--- backend/core/retriever.py
+++ backend/core/retriever.py
@@ -22,7 +22,7 @@ def semantic_retriever(prompt:str):
         embeddings = model(**query_inputs).last_hidden_state.mean(dim=1).squeeze().tolist()
 
     search_result = client.query_points(
-        collection_name="RAG_Collection",
+        collection_name="FusionRAG",
         query=embeddings,
         with_payload=True,
         limit=2
@@ -31,16 +31,18 @@ def semantic_retriever(prompt:str):
     return search_result
 
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
 
-#         prompt = input("Enter a query:")
+        prompt = input("Enter a query:")
 
-#         result = semantic_retriever(prompt)
+        result = semantic_retriever(prompt)
 
-#         # for point in result:
-#         #      print(point.payload['text'])
-#         print(result)
+        # for point in result:
+        #      print(point.payload['text'])
+        print(result)
 
-#         for res in result:
-#              doc_text = res.payload['text']
-#              print(doc_text)
+        for res in result:
+             doc_text = res.payload['text']
+             source = res.payload['source']
+             print(doc_text)
+             print(source)
